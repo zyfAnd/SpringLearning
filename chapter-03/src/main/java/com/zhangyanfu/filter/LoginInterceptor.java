52e3f1907f246b24757bb1cf693ea70ab76f656a
zyfand <1105564280@qq.com>
2018-01-03 23:34:58 +0800

    SpringMVC 拦截器的学习

--- chapter-03/src/main/java/com/zhangyanfu/filter/LoginInterceptor.java
+++ chapter-03/src/main/java/com/zhangyanfu/filter/LoginInterceptor.java
@@ -1,10 +1,13 @@
 package com.zhangyanfu.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @Author Zhang Yanfu
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  * @Email 1105564280@qq.com
  */
 public class LoginInterceptor implements HandlerInterceptor {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     /**
      * 在controller 之前调用
      * 返回值为false 的时候整个请求就结束了
@@ -24,14 +28,22 @@ public class LoginInterceptor implements HandlerInterceptor {
      * @throws Exception
      */
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        return false;
+        HttpSession session = httpServletRequest.getSession();
+        Object user = session.getAttribute("user");
+        if(user==null){
+            logger.info("user===null");
+            httpServletResponse.sendRedirect("/login");
+            return false;
+        }
+        return true;
+
     }
 
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
-
+        logger.info("postHandle------------");
     }
 
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
-
+        logger.info("afterCompletion------------");
     }
 }
---
+++ chapter-03/web/WEB-INF/page/login.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+<form>
+    <input type="text" name="name"/><br>
+    <input type="password" name="pwd"/><br>
+    <input type="submit" value="登录"/>
+</form>
+
+</body>
+</html>
